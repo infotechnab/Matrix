5281f75ef82be5e454f2a225edbce610c3265093
subediramji <ramjisubedi1991@yahoo.com>
2014-01-17 14:13:14 +0545

    matrix is renamed as login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Matrix extends CI_Controller {
+class Login extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
